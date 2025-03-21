b8e44b54946ae7db15e065c8768d4079c15a066b
Giovanni Stasi <gsstasi@gmail.com>
2025-03-21 16:36:31 +0100

    attempt fix

--- client/next.config.js
+++ client/next.config.js
@@ -1,20 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/boot-up",
-        permanent: true,
-      },
-    ];
-  },
   experimental: {
     appDir: false,
   },
   output: "export",
-  basePath: "/giovannistasi.github.io", // Replace with your repo name if needed
   images: {
     unoptimized: true,
-  },
+  }
 };
