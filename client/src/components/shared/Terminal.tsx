a6851707a306f8e5cc10fc5c75c66cb0a8d6ed4e
giovannistasi <gsstasi@gmail.com>
2025-03-21 16:06:59 +0000

    Deploying to gh-pages from @ giovannistasi/giovannistasi.github.io@8241f5f3300e16c85d39ff062db6fe425ba3e833 🚀

--- client/src/components/shared/Terminal.tsx
+++
@@ -1,3 +0,0 @@
-import dynamic from "next/dynamic";
-
-export const Terminal = dynamic(() => import("./XTerm"), { ssr: false });
--- client/src/lib/zIndex.ts
+++
@@ -1,9 +0,0 @@
-const zIndex = {
-  taskbar: 400,
-  appMenu: 300,
-  draggableAppWindow: 200,
-  fullScreenAppWindow: 10,
-  timeWidget: 350,
-};
-
-export default zIndex;
